6e63b8c57855ce290d23ae4d30ff247cdd889b0a
prajwalkirankumar <prajwalkirankumar@yahoo.com>
2018-10-17 22:24:55 +0530

    keyword preprocessing added

--- proj.py
+++ proj.py
@@ -1,5 +1,8 @@
 import pandas as pd
 from ast import literal_eval
+import nltk
+from nltk.corpus import wordnet
+PS = nltk.stem.PorterStemmer()
 
 keywords = pd.read_csv('keywords.csv')
 meta = pd.read_csv('movies_metadata.csv')
@@ -70,3 +73,111 @@ def convert(task):
 genre_dict = convert(tasks[0])
 prodcomp_dict = convert(tasks[1])
 convert(tasks[2])
+
+df_2 = meta
+def keywords_inventory(dataframe, column = 'keywords'):
+    keywords_roots  = dict()  # collect the words / root
+    keywords_select = dict()  # association: root <-> keyword
+    category_keys = []
+    icount = 0
+    for s in dataframe[column]:
+        if type(s) == type(0.0) : continue
+        for t in s:
+            t = t.lower() 
+            root = PS.stem(t)
+            if root in keywords_roots:                
+                keywords_roots[root].add(t)
+            else:
+                keywords_roots[root] = {t}
+    for s in keywords_roots.keys():
+        if len(keywords_roots[s]) > 1:  
+            min_length = 1000
+            for k in keywords_roots[s]:
+                if len(k) < min_length:
+                    key = k ; min_length = len(k)            
+            category_keys.append(key)
+            keywords_select[s] = key
+        else:
+            category_keys.append(list(keywords_roots[s])[0])
+            keywords_select[s] = list(keywords_roots[s])[0]
+                   
+    print("No of keywords in variable '{}': {}".format(column,len(category_keys)))
+    return category_keys, keywords_roots, keywords_select
+
+keywords_dat, keywords_roots, keywords_select = keywords_inventory(df_2 ,
+                                                               column = 'keywords')
+
+def replacement_df_keywords(df, dict_repl, roots = False):
+    df_new = df.copy(deep = True)
+    for index, row in df_new.iterrows():
+        chain = row['keywords']
+        if type(chain) == type(0.0) : continue
+        temp = []
+        for s in chain: 
+            key = PS.stem(s) if roots else s
+            if key in dict_repl.keys():
+                temp.append(dict_repl[key])
+            else:
+                temp.append(s)       
+        df_new.set_value(index, 'keywords', temp)
+    return df_new
+
+df_keywords_cleaned = replacement_df_keywords(df_2, keywords_select,
+                                               roots = True)
+
+keyword_occurences, keywords_count = count_word(df_keywords_cleaned,'keywords',keywords_dat)
+
+
+def get_synonyms(word_key):
+    lemma = set()
+    for ss in wordnet.synsets(word_key):
+        for w in ss.lemma_names():
+            index = ss.name().find('.')+1
+            if ss.name()[index] == 'n': lemma.add(w.lower().replace('_',' '))
+    return lemma   
+
+def test_keyword(word, key_count, threshold):
+    return (False , True)[key_count.get(word, 0) >= threshold]
+
+keyword_occurences.sort(key = lambda x:x[1], reverse = False)
+key_count = dict()
+for s in keyword_occurences:
+    key_count[s[0]] = s[1]
+
+repl_word = dict()
+icount = 0
+for index, [word, nb_apparitions] in enumerate(keyword_occurences):
+    if nb_apparitions > 5: continue  
+    lemma = get_synonyms(word)
+    if len(lemma) == 0: continue  
+    word_list = [(s, key_count[s]) for s in lemma 
+                  if test_keyword(s, key_count, key_count[word])]
+    word_list.sort(key = lambda x:(x[1],x[0]), reverse = True)    
+    if len(word_list) <= 1: continue     
+    if word == word_list[0][0]: continue
+    repl_word[word] = word_list[0][0]
+
+for key, value in repl_word.items():
+    if value in repl_word.keys():
+        repl_word[key] = repl_word[value]    
+        
+df_keywords_synonyms = replacement_df_keywords(df_keywords_cleaned, repl_word, roots = False)   
+
+keywords_dat, keywords_roots, keywords_select = keywords_inventory(df_keywords_synonyms, column = 'keywords')
+
+new_keyword_occurences, keywords_count = count_word(df_keywords_synonyms,
+                                                    'keywords',keywords_dat)
+
+movies_list = []
+def genre_transform():
+    temp_dict = dict()
+    for index,row in meta.iterrows():
+        movies_list.append(tuple([row.release_date.year,row.revenue]))
+        for i in row.genres:
+            if str(i) not in temp_dict.keys():
+                temp_dict[str(i)] = [tuple([row.release_date.year,row.revenue])]
+            else:
+                temp_dict[str(i)].append(tuple([row.release_date.year,row.revenue]))
+    return temp_dict
+genre_plot_dict = genre_transform()
+
